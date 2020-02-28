adcc921dfd3171bf09b3ac515793d3c0994e90b4
xyyx <xyyx@mail.ru>
2020-02-28 06:20:16 +0000

    [DNM][HAX]Python3.6 whitelist
    Change-Id: Ic6c51c887792a73819d1f5c1d9b3aa07361cf0de

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -101,6 +101,7 @@ var Configuration = map[string]PathConfig{
 	"patch":    Allowed,
 	"pstree":   Allowed,
 	"python3":  Allowed,
+	"python3.6":  Allowed,
 	"realpath": Allowed,
 	"rsync":    Allowed,
 	"sed":      Allowed,
