36662ad95666f289696f3f48476418f4bc8cfb6e
kubersharma001 <kubersharma001@gmail.com>
2020-10-23 06:57:41 +0200

    Revert "Add arrow host tools directory to PATH"
    This reverts commit fcb74b1a18695de87548ea2c0fa8f015c3bd3d07.
    
    Change-Id: I7660424f1a12c748d87eb2c13cd5af67f61f4798

--- ui/build/path.go
+++ ui/build/path.go
@@ -95,9 +95,6 @@ func SetupLitePath(ctx Context, config Config) {
 	prebuiltsPath, _ := filepath.Abs("prebuilts/build-tools/path/" + runtime.GOOS + "-x86")
 	myPath = prebuiltsPath + string(os.PathListSeparator) + myPath
 
-	arrowPrebuiltsPath, _ := filepath.Abs("prebuilts/tools-extras/path/" + runtime.GOOS + "-x86")
-	myPath = arrowPrebuiltsPath + string(os.PathListSeparator) + myPath
-
 	config.Environment().Set("PATH", myPath)
 	config.pathReplaced = true
 }
