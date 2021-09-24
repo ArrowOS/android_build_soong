16fa507b09440ae87381e6bff3ce18cd7c4365a5
kubersharma001 <kubersharma001@gmail.com>
2021-09-24 08:17:32 +0000

    fixup! soong: Add PathForSourceRelaxed
    Change-Id: I9f3a7e86802d7b372937974b594ac88d085daacd

--- android/paths.go
+++ android/paths.go
@@ -984,7 +984,7 @@ func pathForSource(ctx PathContext, pathComponents ...string) (SourcePath, error
 // It differs from pathForSource in that the path is allowed to exist outside of the PathContext.
 func pathForSourceRelaxed(ctx PathContext, pathComponents ...string) (SourcePath, error) {
 	p := filepath.Join(pathComponents...)
-	ret := SourcePath{basePath{p, ""}, ctx.Config().srcDir}
+	ret := SourcePath{basePath{p, ""}, "."}
 
 	abs, err := filepath.Abs(ret.String())
 	if err != nil {
