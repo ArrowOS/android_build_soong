a9da6f3919e7d3591db5d7e1390e19a38a612f67
Arian <arian.kulmer@web.de>
2022-09-13 13:04:19 +0200

    Add perl to allowed host tools
    This is necessary to build external qcom kernel modules.
    This matches CLO: https://git.codelinaro.org/clo/la/platform/build/soong/-/blob/LA.QSSI.12.0.r1-08700.02-qssi.0/ui/build/paths/config.go#L97
    
    Change-Id: Id1e4dd43db89140c95fc81912494ac36e68c031f

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -92,6 +92,7 @@ var Configuration = map[string]PathConfig{
 	"lsof":    Allowed,
 	"nproc":   Allowed,
 	"openssl": Allowed,
+	"perl":    Log,
 	"pstree":  Allowed,
 	"rsync":   Allowed,
 	"sh":      Allowed,
